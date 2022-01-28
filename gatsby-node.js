a7f46f5518e87ddc46abd0529f314ded75919690
1 <a1@MacBook-Air-1.local>
2022-01-28 14:18:03 +0300

    fix gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,13 @@
-exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators
-
-  const blogPostTemplate = path.resolve(`src/posts/post.js`)
-
-  return graphql(`
-    {
-      query
-      MyQuery {
-        allDatoCmsPost) {
+const path = require("path")
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  // Query for markdown nodes to use in creating pages.
+  const result = await graphql(
+    `
+      {
+        allDatoCmsPost {
           edges {
             node {
               title
@@ -19,18 +19,23 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
           }
         }
       }
-    }
-  `).then(result => {
-    if (result.errors) {
-      return Promise.reject(result.errors)
-    }
-
-    result.data.allDatoCmsPost.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: blogPostTemplate,
-        context: {}, // additional data can be passed via context
-      })
+    `
+  )
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+  // Create pages for each markdown file.
+  const blogPostTemplate = path.resolve(`src/pages/posts/{DatoCmsPost.slug}.js`)
+  result.data.allDatoCmsPost.edges.forEach(({ node }) => {
+    const path = node.slug
+    createPage({
+      path,
+      component: blogPostTemplate,
+      // In your blog post template's graphql query, you can use pagePath
+      // as a GraphQL variable to query for data from the markdown file.
+      context: {},
     })
   })
 }
