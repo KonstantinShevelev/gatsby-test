53c2692134b1162d7ebd4662fcd6065cc6168f6a
konstantin.sh <konstantin.sh@ramotion.agency>
2022-03-14 14:53:44 +0300

    add fixes for sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,21 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: process.env.SITE_URL
   },
   plugins: [
-    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-advanced-sitemap`,
+      options: {
+        output: "/sitemap.xml",
+      }
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
